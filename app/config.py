ce3d1a5dc8169480f8ece930856c39772800dafd
Davranaff <davranaff@192.168.0.77>
2025-08-20 18:10:40 +0500

    Add initial structure

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Core Service"
+    DEBUG: bool = True
+
+    # DB
+    DATABASE_URL: str
+
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
